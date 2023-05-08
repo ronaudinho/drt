44c43456113ad48c21b0d15ebefe902a17e1ef33
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2023-05-08 17:04:27 +0700

    feat: add integration with open dota api

---
+++ rsc/client/opendota/match.go
@@ -0,0 +1,58 @@
+package opendota
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+var (
+	ErrFetchMatchDetail = fmt.Errorf("failed to fetch match detail")
+)
+
+type OpenDotaAPI struct {
+	httpClient *http.Client
+	APIURL     string
+}
+
+func NewMatchAPI(httpClient *http.Client, APIURL string) *OpenDotaAPI {
+	return &OpenDotaAPI{
+		httpClient: httpClient,
+		APIURL:     APIURL,
+	}
+}
+
+func (m *OpenDotaAPI) FetchMatchDetail(ctx context.Context, matchID int64) (MatchDetail, error) {
+	matchDetailURL := fmt.Sprintf("%s/matches/%d", m.APIURL, matchID)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, matchDetailURL, nil)
+	if err != nil {
+		return MatchDetail{}, err
+	}
+
+	res, err := m.httpClient.Do(req)
+	if err != nil {
+		return MatchDetail{}, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return MatchDetail{}, ErrFetchMatchDetail
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return MatchDetail{}, err
+	}
+
+	var matchDetail MatchDetail
+
+	err = json.Unmarshal(body, &matchDetail)
+	if err != nil {
+		return MatchDetail{}, err
+	}
+
+	return matchDetail, nil
+}
---
+++ rsc/client/opendota/match_integration_test.go
@@ -0,0 +1,44 @@
+//go:build integration
+// +build integration
+
+package opendota
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFetchMatchDetail(t *testing.T) {
+	ctx := context.Background()
+	t.Run("fetch match detail dota2 should be success", func(t *testing.T) {
+		openDotaAPIURL := "https://api.opendota.com/api"
+		httpClient := http.DefaultClient
+
+		openDotaAPI := NewMatchAPI(httpClient, openDotaAPIURL)
+
+		// my personal match id
+		matchID := 271145478
+
+		matchDetail, err := openDotaAPI.FetchMatchDetail(ctx, int64(matchID))
+
+		assert.NoError(t, err)
+		assert.NotEmpty(t, matchDetail)
+	})
+
+	t.Run("fetch match detail dota2 should failed because match id is not found", func(t *testing.T) {
+		openDotaAPIURL := "https://api.opendota.com/api"
+		httpClient := http.DefaultClient
+
+		openDotaAPI := NewMatchAPI(httpClient, openDotaAPIURL)
+
+		matchID := 0
+
+		matchDetail, err := openDotaAPI.FetchMatchDetail(ctx, int64(matchID))
+
+		assert.Error(t, err)
+		assert.Empty(t, matchDetail)
+	})
+}
